4f29f0aea21d9eda078292829ed6c900676d48f3
codac-dev <codac-dev@ft-srv-2.codac.local>
2020-08-03 11:31:24 +0200

    Define StateMachineEvents as permament MessageFilters

--- Source/Core/Scheduler/L4StateMachine/StateMachine.cpp
+++ Source/Core/Scheduler/L4StateMachine/StateMachine.cpp
@@ -242,9 +242,6 @@ ErrorManagement::ErrorType StateMachine::EventTriggered(ReferenceT<StateMachineE
                 }
             }
         }
-	else {
-            err = InstallMessageFilter(event);
-	}
     }
     else {
         REPORT_ERROR(ErrorManagement::FatalError, "In state (%s) the next state is not valid", currentState->GetName());
