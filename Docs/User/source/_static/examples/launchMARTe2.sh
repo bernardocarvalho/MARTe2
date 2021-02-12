b60b103d1bc3d791a47a3020b7f1a474b4273d8b
node07-developer <cristian.galperti@epfl.ch>
2021-02-12 16:38:38 +0100

    Code Ver String written to MDS for the first time

--- Docs/User/source/_static/examples/launchMARTe2.sh
+++ Docs/User/source/_static/examples/launchMARTe2.sh
@@ -1,2 +1,3 @@
 #./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/AFHBA2GTester1.cfg  -m ShotManager:Start
-./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkWrapperTester.cfg  
+#./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkWrapperTester.cfg  
+./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkCodeVerTester.cfg 
