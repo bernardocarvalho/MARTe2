5f0bc4d6597d26fd6748532eb3c378211a12e7a0
node07-developer <cristian.galperti@epfl.ch>
2021-03-11 10:19:37 +0100

    before merging into devel-spc

--- Docs/User/source/_static/examples/launchMARTe2.sh
+++ Docs/User/source/_static/examples/launchMARTe2.sh
@@ -1,3 +1,4 @@
 #./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/AFHBA2GTester1.cfg  -m ShotManager:Start
+./MARTeApp.sh -l SPCRealTimeLoader -f /home/tcv_oper/martecfgs/1005/node07-1005-gen.cfg  -m ShotManager:Start
 #./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkWrapperTester.cfg  
-./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkCodeVerTester.cfg 
+#./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkCodeVerTester.cfg 
